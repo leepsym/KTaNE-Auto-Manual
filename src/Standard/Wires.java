b33dd5a52fd499a704f0f99facc6e0d0363f2f8c
leepsym <frazeth@whs.school.nz>
2025-05-01 15:19:14 +1200

    0.0.4
    Wires.java functionality, still in progress.

--- src/Standard/Wires.java
+++ src/Standard/Wires.java
@@ -6,18 +6,51 @@ import Main.Static;
 public class Wires {
     public Wires() {
         Static.window.query("How many wires?", new Option[]{
-                new Option("3", l -> three()),
-                new Option("4", l -> four()),
+                new Option("3", l -> three(0)),
+                new Option("4", l -> four(0)),
                 new Option("5", l -> five()),
                 new Option("6", l -> six())
         });
     }
-    private void three() {
-        System.out.println("3");
+    private void three(int state) {
+        switch (state) {
+            case 0 ->
+                Static.window.query("Are there any red wires?", new Option[]{
+                        new Option("Yes", l -> three(1)),
+                        new Option("No", l -> Static.window.query("Cut the second wire.", new Option[]{
+                                new Option("Return", i -> Static.window.returnToModules())
+                        }))
+                });
+
+            case 1 ->
+                Static.window.query("Is the last wire white?", new Option[]{
+                        new Option("Yes", l -> Static.window.query("Cut the last wire.", new Option[]{
+                                new Option("Return", i -> Static.window.returnToModules())}
+                        )),
+                        new Option("No", l -> three(2))
+                });
+
+            case 2 ->
+                Static.window.query("Is there more than one blue wire?", new Option[]{
+                        new Option("Yes", l -> Static.window.query("Cut the last blue wire.", new Option[]{
+                                new Option("Return", i -> Static.window.returnToModules())}
+                        )),
+                        new Option("No", l -> Static.window.query("Cut the last wire.", new Option[]{
+                                new Option("Return", i -> Static.window.returnToModules())}
+                        ))
+                });
+
+            default ->
+                System.out.println("Incorrect input case Standard/Wires.wiresVisualization");
+        }
     }
 
-    private void four() {
-        System.out.println("4");
+    private void four(int state) {
+        switch (state) {
+            case 0 ->
+                Static.window.query("Is there more than one red wire?", new Option[]{
+                        new Option("Yes", l -> Static.window.query("Is the last digit of the serial number odd?", new Option[]{new Option("Yes", l -> Static.window.query("Cut the last red wire.", new Option[]{new Option("Return", i -> Static.window.returnToModules())}))})});
+        }
     }
 
     private void five() {
